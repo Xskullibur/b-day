09402b4fe967a4c01911b49c71b2a84608adfe40
meow.yoshino@yahoo.com <meow.yoshino@yahoo.com>
2019-08-11 18:15:27 +0000

    update

--- routes/main.js
+++ routes/main.js
@@ -8,14 +8,17 @@ var moment = require('moment')
 
  const INFO = {
     eventDate: moment('2019-09-27').format('Do MMMM YYYY'),
-    eventTime: '20:00 - 02:00',
+    eventTime: '20:30 - 02:00',
     eventVenue: 'Arcade Warehouse'
  }
 
 const PLAYERS = [
-  "Joel", "Shawn", "Jonah", "Ryan", "James", "Tee Chin", "Damien", "Raphel", "Zheng Teck", "Chen Yee",
-  "Nat Sia", "Lionel", "Sam Fang", "Sam Lee", "Jolyn", "Yusuf", "Tudy", "Jedrick", "Le Cong", "Wenjing",
-  "Chia Feng", "Andrew", "Wen Qing", "Wen Hong", "Jing Sheng", "Lindy"
+  "Joel", "Shawn", "Jonah", "Ryan", "James", "Tee Chin", "Damien", "Raphel", "Zheng Teck", "Lance",
+  "Jolyn", "Yusuf", "Tudy", "Jedrick", "Le Cong", "Wenjing",
+  "Chia Feng", "Andrew", "Wen Qing", "Wen Hong", "Hong Yi",
+  "Jayden", "Hui Xin", "Lindy",
+  "Sam Fang", "Sam Lee", "Chen Yee", "Nat Sia", "Lionel", "Wei Feng",
+	"Alson", "Alwyn", "Jayna"
 ]
 
 router.use((req, res, next) => {
@@ -39,8 +42,8 @@ router.get('/', function(req, res, next) {
 
 router.post('/login', function(req, res) {
 
-  if (req.body.passcode && PLAYERS.includes(req.body.passcode)) {
-    let userName = req.body.passcode
+  if (req.body.passcode && PLAYERS.map(a => a.toLowerCase()).includes(req.body.passcode.toLowerCase())) {
+    let userName = req.body.passcode.toUpperCase()
     let status = "LIVE"
 
     res.render('index', {
